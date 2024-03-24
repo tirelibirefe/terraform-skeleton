60c541196e3af6c4d1fcd5d8fa3f13e8c337fc18
Tireli Efe <tirelibirefe@gmail.com>
2024-03-24 07:54:17 +0300

    provider was added.

--- provider.tf
+++ provider.tf
@@ -1,6 +1,15 @@
 #------------------------------------------------------------------------------
 # Setup the Provider
 #------------------------------------------------------------------------------
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.42.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
